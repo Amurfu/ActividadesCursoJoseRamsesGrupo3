5d30038db700cd73df2579e5b7eec6297b4ee994
jose ramses lopez <ramses12master@gmail.com>
2023-11-11 11:08:06 -0600

    Se agrega contexto de seguridad

---
+++ Actividades/Actividad 4 Spring/Ejemplo/tienda/src/main/java/com/amurfu/tienda/data/dto/CredencialesDto.java
@@ -0,0 +1,12 @@
+package com.amurfu.tienda.data.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CredencialesDto {
+
+    private String usuario;
+    private String contraseña;
+}
